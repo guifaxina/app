0ae7836043adb78ba7c123be97738a43f4c01d53
Guilherme Faxina <gamerstarbr7@hotmail.com>
2022-11-10 13:23:19 -0300

    refactor: improved auth ralated responses

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,17 +1,17 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { Request, Response, NextFunction, json } from "express";
+import { Request, Response, NextFunction } from "express";
 
 
 function auth(req: Request, res: Response, next: NextFunction) {
   const token = req.header("authorization");
-  if (!token) return res.status(401)
+  if (!token) return res.status(400).json({ status: "Error", message: "Token not found" })
 
   const isUserVerified = jwt.verify(token!, process.env.TOKEN_SECRET) as JwtPayload;
   const isAdmin = isUserVerified.admin
   if (isAdmin) {
     next()
   } else {
-    res.sendStatus(400)
+    res.status(401).json({ status:"Failed", message: "Unauthorized." })
   }
 }
 
